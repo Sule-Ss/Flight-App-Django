07441d36cef00569e198cb10f271b82d4f6f5355
Sule <aybilenden@gmail.com>
2022-06-22 21:44:05 +0300

    added

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
-from django.contrib import admin
 from django.urls import path, include
 
+from users.views import RegisterView
+
 urlpatterns = [
-    path('dj-rest-auth/', include('dj_rest_auth.urls'))
+    path('auth/', include('dj_rest_auth.urls')),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
