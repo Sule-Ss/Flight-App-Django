630016b20b15eab1a9ba562c27773098405b0fba
Sule <aybilenden@gmail.com>
2022-06-23 22:22:39 +0300

    added

--- flight/serializers.py
+++ flight/serializers.py
@@ -7,22 +7,17 @@ class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
         fields = (
+            "id",
             "flight_number",
             "operation_airlines",
             "departure_city",
             "arrival_city",
             "date_of_departure",
-            "etd",
+            "etd"
         )
 
-class ReservationSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Reservation
-        fields = '__all__'
 
 class PassengerSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Passenger
         fields = '__all__'
@@ -31,17 +26,22 @@ class PassengerSerializer(serializers.ModelSerializer):
 class ReservationSerializer(serializers.ModelSerializer):
 
     passenger = PassengerSerializer(many=True, required=False)
-    serializer = serializers.StringRelatedField() #default read_only = True
-    user = serializers.StringRelatedField() #default read_only = True
+    flight = serializers.StringRelatedField()  # default read_only=True
+    user = serializers.StringRelatedField()  # default read_only=True
     flight_id = serializers.IntegerField(write_only=True)
-    user_id = serializers.IntegerField(write_only = True, required = False)
+    user_id = serializers.IntegerField(write_only=True, required=False)
 
-
-    class Meta :
+    class Meta:
         model = Reservation
         fields = (
-            'id',
-            'flight',
-            'user',
-            'passenger',
+            "id",
+            "flight",
+            "flight_id",
+            "user",
+            "user_id",
+            "passenger"
         )
+
+    def create(self, validated_data):
+        passenger_data = validated_data.pop('passenger')
+        validated_data['user_id'] = self.context['request'].user.id
