ff49b59890b2d3afedf65d42ef42f12f779e6ed3
Sule <aybilenden@gmail.com>
2022-06-23 21:40:07 +0300

    added

--- flight/urls.py
+++ flight/urls.py
@@ -1,10 +1,9 @@
-from django.urls import include, path
 from rest_framework import routers
-from .views import FlightView
-
+from .views import FlightView, ReservationView
 
 router = routers.DefaultRouter()
 router.register('flights', FlightView)
+router.register('resv', ReservationView)
 
 
 urlpatterns = [
--- flight/views.py
+++ flight/views.py
@@ -1,11 +1,18 @@
+from django import views
 from django.shortcuts import render
-from .serializers import FlightSerializer
+from .serializers import FlightSerializer, ReservationSerializer
 from .models import Flight, Passenger, Reservation
 from rest_framework import viewsets
+from .permission import IsStaffforReadOnly
 
 
 class FlightView(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+    permission_classes = (IsStaffforReadOnly,)
 
-    
+
+class ReservationView(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
